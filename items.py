106ff3ab712a86749ee93071b61a72b0c2115fac
allolgeil <2806354774@qq.com>
2017-08-18 11:23:53 +0800

    Update items.py

--- items.py
+++ items.py
@@ -1,3 +1,12 @@
 import scrapy
 
 class Jdgoods Item(scrapy.Item):
+  pdnum = scrapy.Field()
+  pd1 = scrapy.Field()
+  pd2 = scrapy.Field()
+  name = scrapy.Field()
+  price = scrapy.Field()
+  pnum = scrapy.Field()
+  author = scrapy.Field()
+  pub = scrapy.Field()
+  seller = scrapy.Field()
